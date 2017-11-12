45f170464542bca68d2ff46cae3e42f4976b8d81
Claudius Mbemba <mbembac@gmail.com>
2017-11-12 08:46:54 -0800

    messages and validations arriving in appropriate order

---
+++ src/example/spec/hiScript.2.ts
@@ -0,0 +1,20 @@
+export =[
+  {
+    "user": "hi"
+  },
+  {
+    "bot": "Nice to meet you!"
+  },
+  {
+    "bot": "Ok time to go!"
+  },
+  {
+    "bot": "Are you ready to leave?"
+  },
+  {
+    "user": "See you"
+  },
+  {
+    "bot": "Goodbye!"
+  }
+];
